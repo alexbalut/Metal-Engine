774390540aa06f64215082ba2d81acbf750bd6aa
alexbalut <124917198+alexbalut@users.noreply.github.com>
2024-01-28 15:42:58 -0500

    Initial Commit

---
+++ Metal Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Metal Engine
+//
+//  Created by Alexander Balut on 2024-01-28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
